544df82cd2ba92edbe00a020e9f5d5a16fcd1853
William Bae <williambae1@gmail.com>
2017-09-01 13:19:26 +0900

    New pretty-print compile task

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,19 @@ gulp.task('compile', function() {
 		.pipe(gulp.dest('./dist/'))
 })
 
+gulp.task('compile-pretty', function() {
+	return gulp.src('./src/aheui.js', {base: './'})
+		.pipe(closureCompiler({
+			compilation_level: 'SIMPLE', // TODO ADVANCED
+			warning_level: 'VERBOSE',
+			language_in: 'ECMASCRIPT6_STRICT',
+			language_out: 'ECMASCRIPT5_STRICT',
+			js_output_file: 'aheui.min.js',
+			formatting: 'pretty_print'
+		}))
+		.pipe(gulp.dest('./dist/'))
+})
+
 gulp.task('test', function(callback) {
 	var child = child_process.exec('./test/snippets-standard.sh', callback)
 	child.stdout.pipe(process.stdout)
