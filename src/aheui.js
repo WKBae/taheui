34050fb73d239253298527354787158cc3d022d4
William Bae <williambae1@gmail.com>
2017-09-05 00:48:47 +0900

    Common count check instead of blind loops
    Most(actually, all in current code) pops one or two items. Will help by returning specialized functions for them without loops.

--- src/aheui.js
+++ src/aheui.js
@@ -141,20 +141,47 @@
 	function popOperation(count, operation, resultHandler) {
 		var handler = resultHandler || ((stack, result, argument) => stack.push(result))
 
-		return function popOperate(stack, argument) {
-			/** @type {Array<number>} */
-			var args = []
-			for(var i = count - 1; i >= 0; i--) {
-				args[i] = stack.pop()
-				if(args[i] === undefined) {
-					var push = stack.append || stack.push // (stack instanceof Queue)? stack.append : stack.push
-					for(i++; i < count; i++) {
-						push.call(stack, args[i])
-					}
+		if(count == 1) {
+			return function pop1Operate(stack, argument) {
+				/** @type {number|undefined} */
+				var one = stack.pop()
+				if(one === undefined) {
 					return false
 				}
+				return handler(stack, operation(one), argument)
 			}
-			return handler(stack, operation.apply(this, args), argument)
+		} else if(count == 2) {
+			return function pop2Operate(stack, argument) {
+				/** @type {number|undefined} */
+				var two = stack.pop()
+				if(two === undefined) {
+					return false
+				}
+				/** @type {number|undefined} */
+				var one = stack.pop()
+				if(one === undefined) {
+					if(stack.append) stack.append(two)
+					else stack.push(two)
+					return false
+				}
+				return handler(stack, operation(one, two), argument)
+			}
+		} else {
+			return function popOperate(stack, argument) {
+				/** @type {Array<number>} */
+				var args = []
+				for(var i = count - 1; i >= 0; i--) {
+					args[i] = stack.pop()
+					if(args[i] === undefined) {
+						var push = stack.append || stack.push // (stack instanceof Queue)? stack.append : stack.push
+						for(i++; i < count; i++) {
+							push.call(stack, args[i])
+						}
+						return false
+					}
+				}
+				return handler(stack, operation.apply(this, args), argument)
+			}
 		}
 	}
 
